dca84ea72a2ce4a82ae743a1e55d561775acc3f6
Dominik Janák <dev@dominikjanak.cz>
2021-10-31 01:21:08 +0200

    Add page router

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 This package is used to support local development on my computers. It contains a list of useful services and links in one place.
 
+The application uses a simple router to show different pages. `example.org/name` will use a source from `pages/name.md`. The default route `/` is the alias to `index` with source in `pages/index.md` file. 
+
+### Special md-source tags
+|     Tag     |   Rewrite   |
+| ----------- | ----------- |
+|`::br::` | &lt;br /&gt; |
+
+### Warning
+MD source may not contains HTML code!
+
 ## License
-2021 - MIT Dominik Janák
+2021 - MIT - Dominik Janák
--- index.php
+++ index.php
@@ -4,56 +4,93 @@ require_once('libs/TinyHtmlMinifier.php');
 
 umask(2);
 
-$parsedown = new Parsedown();
-$parsedown->setSafeMode(true);
-$parsedown->setMarkupEscaped(true);
+try {
 
-if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
-    die("Only local user allowed!");
-}
+    $parsedown = new Parsedown();
+    $parsedown->setSafeMode(true);
+    $parsedown->setMarkupEscaped(true);
 
-$compile = true;
+    if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+        die("Only local user allowed!");
+    }
 
-$source = "./dashboard_content.md";
-$template = './templates/dashboard/template.html';
-$cache = "./cache/index.cache.html";
-$lastModification = "./cache/index-last-modification.txt";
+    $compile = false;
 
-if (!$compile && !file_exists($cache) && (!file_exists($source) || !file_exists($template))) {
-    die("<h2 style='color: #cf222e'>#1 - Unexpected error!</h2>");
-}
+    $conf = [
+        'pages_source' => 'pages',
+        'index' => 'index',
+        'type' => 'md',
+        'template' => 'templates/dashboard/template.html',
+        'cache' => 'cache'
+    ];
 
-// next get xor generate the cache
+    // router
+    $route = trim($_SERVER['REQUEST_URI'], '/');
 
-if (!$compile && file_exists($cache) && file_exists($lastModification)
-    && file_get_contents($lastModification) == filemtime($source)) {
-    $output = file_get_contents($cache);
+    if(empty($route)) {
+        $route = $conf['index'];
+    }
+
+    $source = sprintf("%s/%s.%s",
+        $conf['pages_source'],
+        $route,
+        $conf['type']
+    );
+
+    $cache=sprintf("%s/%s",
+        $conf['cache'],
+        $conf['pages_source']
+    );
+
+    $cacheFile = sprintf('%s/%s.cache.html', $cache, $route);
+    $lastModFile = sprintf('%s/%s.mod.txt', $cache, $route);
+
+    if (!file_exists($cacheFile) && (!file_exists($source) || !file_exists($conf['template']))) {
+        die("<h2 style='color: #cf222e'>Route not found!</h2>");
+    }
+
+    // next get xor generate the cache
+
+    if (!$compile && file_exists($cacheFile) && file_exists($lastModFile)
+        && file_get_contents($lastModFile) == filemtime($source)) {
+        $output = file_get_contents($cacheFile);
+        die($output);
+    }
+
+    // gen
+    if ($compile || (file_exists($conf['template']) && file_exists($source))) {
+        $md = file_get_contents($source);
+        $html = $parsedown->text($md);
+        $template = file_get_contents($conf['template']);
+        $html = str_replace([
+            "{{content}}",
+            "::br::"
+        ], [
+            $html,
+            '<br/>'
+        ], $template);
+
+        $minifier = new TinyHtmlMinifier(['collapse_whitespace' => true, 'disable_comments' => true]);
+        $html = $minifier->minify($html);
+
+        if(!file_exists($cache)) {
+            mkdir($cache);
+        }
+
+        file_put_contents($cacheFile, $html);
+        file_put_contents($lastModFile, filemtime($source));
+        $output = $html;
+    }
+
+    if (empty($output)) {
+        $output = file_get_contents($cacheFile);
+    }
+
     die($output);
 }
-
-// gen
-if ($compile || (file_exists($template) && file_exists($source))) {
-    $md = file_get_contents($source);
-    $html = $parsedown->text($md);
-    $template = file_get_contents($template);
-    $html = str_replace([
-        "{{content}}",
-        "::br::"
-    ], [
-        $html,
-        '<br/>'
-    ], $template);
-
-    $minifier = new TinyHtmlMinifier(['collapse_whitespace' => true, 'disable_comments' => true]);
-    $html = $minifier->minify($html);
-
-    file_put_contents($cache, $html);
-    file_put_contents($lastModification, filemtime($source));
-    $output = $html;
+catch (Exception $ex)
+{
+    $message = $ex->getMessage();
+    $trace = $ex->getTraceAsString();
+    die("<h2 style='color: #cf222e'>Unexpected error: $message<br />$trace!</h2>");
 }
-
-if (empty($output)) {
-    $output = file_get_contents($cache);
-}
-
-die($output);
